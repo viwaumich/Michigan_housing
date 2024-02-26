a6dbc01d8f7bb47a64a85c671edd1a15fcf5abe9
hessakh <hessakh@umich.edu>
2024-02-26 15:29:05 -0500

    added legislative district names to markers

--- app.py
+++ app.py
@@ -1,25 +1,62 @@
 from shiny import *
 from shinywidgets import output_widget, render_widget
 import ipyleaflet as L
+from ipyleaflet import Map, GeoJSON, LayersControl, WidgetControl
+from ipywidgets import Label
 import shapely
+from shapely.geometry import Point
 from branca.colormap import linear
 import matplotlib.pyplot as plt
 import math
-#import shapely
+import json
 import pandas as pd
 import geopandas as gpd
+import requests
+from geopy.geocoders import Nominatim
+
+
+
+# Step 1: Geocoding the address using OpenStreetMap's Nominatim
+def geocode_address(address):
+    geolocator = Nominatim(user_agent="your_application_name")
+    location = geolocator.geocode(address)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
+
+# Step 2: Check the legislative district
+# Step 2: Check the legislative district
+def check_legislative_district(lat, lng, districts_geojson):
+    point = Point(lng, lat)
+    # Load the legislative districts GeoJSON file
+    districts = gpd.read_file(districts_geojson)
+    
+    # Check which district the point is in
+    for _, district in districts.iterrows():
+        if point.within(district['geometry']):  # 'geometry' is a special key used by GeoPandas
+            # Returning the entire row as a dictionary
+            return district['NAME']
+    
+    return None
 
 path2folder = r"./data/" # fill in the path to your folder here.
 assert len(path2folder) > 0
 
 mhvillage_df = pd.read_csv(path2folder + r"MHVillageAll_Dec7_dropna.csv")
 lara_df = pd.read_excel(path2folder + r"LARA_with_coord.xlsx")
+# Path to your legislative districts GeoJSON file
+house_districts_geojson_path = "/Users/hkt/Michigan_housing/Michigan_State_House_Districts_2021.geojson"
+senate_districts_geojson_path = "/Users/hkt/Michigan_housing/Michigan_State_Senate_Districts_2021.geojson"
 #tracts_shapefile = gpd.read_file(path2folder+r"tl_2019_26_tract.shp")
 
 circlelist = []
 mklist = []
 upper_layers = []
 lower_layers = []
+
+
+
 
 def extract_coordinates(df):
     def rev(lst):
@@ -66,22 +103,35 @@ def build_marker_layer():
     for ind in range(len(mhvillage_df)):
         lon = float(mhvillage_df['Longitude'].iloc[ind])
         lat = float(mhvillage_df['Latitude'].iloc[ind])
-    
+        if lat and lon:
+            # Check the legislative district
+            district_house = check_legislative_district(lat, lon, house_districts_geojson_path)
+            district_senate = check_legislative_district(lat, lon, senate_districts_geojson_path)
+
         markeri = L.Marker(
             location=(lat,lon),
             draggable=False,
             title=str(mhvillage_df['Name'].iloc[ind])+
             ' , number of sites: '+str(mhvillage_df['Sites'].iloc[ind])+
             ' , average rent: '+str(mhvillage_df['Average_rent'].iloc[ind])+
+            ' , House district: '+str(district_house)+
+            ' , Senate district: '+str(district_senate)+
             ' , url: %s'%str(mhvillage_df['Url'].iloc[ind]) +
             ' , MHVillage')
         circlei = L.Circle(location=(lat,lon), radius=1, color="blue", fill_color="blue")
 
         circlelist.append(circlei)
         mklist.append(markeri) 
+
+
     for ind in range(len(lara_df)):
         lon = float(lara_df['Longitude'].iloc[ind])
         lat = float(lara_df['Latitude'].iloc[ind])
+        if lat and lon:
+            # Check the legislative district
+            district_house = check_legislative_district(lat, lon, house_districts_geojson_path)
+            district_senate = check_legislative_district(lat, lon, senate_districts_geojson_path)
+
         if lon == 0 and lat == 0:
             continue
         markeri = L.Marker(
@@ -89,6 +139,8 @@ def build_marker_layer():
             draggable=False,
             title=str(lara_df['Owner / Community_Name'].iloc[ind])+
             ' , number of sites: '+str(lara_df['Total_#_Sites'].iloc[ind])+
+            ' , House district: '+str(district_house)+
+            ' , Senate district: '+str(district_senate)+
             ', LARA')
         circlei = L.Circle(location=(lat,lon), radius=1, color="blue", fill_color="blue")
 
